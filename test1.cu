c9a06e2ebd910679ebd4dd38749f18969123be04
Daria Kokot <dariakokot@student.agh.edu.pl>
2026-03-16 10:20:52 +0100

    Solution

--- test1.cu
+++ test1.cu
@@ -18,7 +18,16 @@
 // C[row][col] = sum over k of A[row][k] * B[k][col]
 // Row-major layout: element (i,j) is at index [i * n + j].
 __global__ void matmul_kernel(const float *A, const float *B, float *C, int n) {
-    // YOUR CODE HERE
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if ( row < n && col < n) {
+        float sum = 0.0f;
+        for ( int k = 0; k < n; ++k){
+            sum += A[row*n+k]*B[k*n+col];
+        }
+        C[row*n + col] = sum;
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -48,6 +57,9 @@ int main(int argc, char *argv[]) {
     // dim3 block(?, ?);
     // dim3 grid(?, ?);
     // YOUR CODE HERE
+    dim3 block(8,8,8);
+    dim3 grid((n+block.x-1)/block.x,
+            (n+block.y-1)/block.y);
 
     cudaEvent_t start, stop;
     CUDA_CHECK(cudaEventCreate(&start));
@@ -56,6 +68,8 @@ int main(int argc, char *argv[]) {
     // TODO: Launch matmul_kernel with <<<grid, block>>>
     CUDA_CHECK(cudaEventRecord(start));
     // YOUR CODE HERE
+    matmul_kernel<<<grid, block>>>(d_A,d_B,d_C, n);
+
     CUDA_CHECK(cudaEventRecord(stop));
     CUDA_CHECK(cudaEventSynchronize(stop));
 
